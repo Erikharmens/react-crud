65f0ab6973c8b1e42f6f321c1ac5c9e7d985e836
Erikharmens <54328933+Erikharmens@users.noreply.github.com>
2021-03-10 12:38:27 +0100

    added [] on useEffect - ProductsList.js

--- src/components/dashboard/products/ProductsList.js
+++ src/components/dashboard/products/ProductsList.js
@@ -23,7 +23,7 @@ export default function ProductsList() {
       }
     }
     getAllProducts();
-  });
+  }, []);
 
   if (loading) return <div>Loading..</div>;
   if (error) return <div>An error occured..</div>;
